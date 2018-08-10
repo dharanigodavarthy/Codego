9e43373dc2a59e0224efe7f2ed981e2a218b45f7
Anmol <anmol.sharma@mountblue.io>
2018-08-10 14:11:54 +0530

    Done login css part

--- src/components/login/SignUp.js
+++ src/components/login/SignUp.js
@@ -1,67 +1,96 @@
-import React from 'react';
+import React from "react";
 // import '../css/login.css';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from "react-router-dom";
 
-class SignUp extends React.Component{
-    state={
-        userName:'',
-        password:'',
-        redirect:false
-    }
-    setName(e){
-        console.log(e.target.value);
-        this.setState({userName:e.target.value});
-    }
+class SignUp extends React.Component {
+  state = {
+    userName: "",
+    password: "",
+    redirect: false
+  };
+  setName(e) {
+    console.log(e.target.value);
+    this.setState({ userName: e.target.value });
+  }
 
-    setPassword(e){
-        console.log(e.target.value);
-        this.setState({password:e.target.value})
-    }
+  setPassword(e) {
+    console.log(e.target.value);
+    this.setState({ password: e.target.value });
+  }
 
-    userValidation(e){
-        console.log(this.state);
-        console.log("login button is clicked!!");   
-        fetch("/auth/users", {
-            method: "POST",
-            body: JSON.stringify({ data: this.state }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(response => response.json())
-        .then(body => {
-            console.log(body[0].userName);
-            console.log(this.state.userName);
-            if (body[0].userName) {
-                this.setState({redirect:true});
-                console.log(this.state);
-                localStorage.setItem("userData", JSON.stringify({name:body[0].userName}));      
-            }
-        })
-        .then(
-            this.setState({
-                userName:'',
-                password:''
-            })
-        )
-    }
-
-    render(){
-        if (this.state.redirect) {
-            console.log("inside redirect");
-            
-            return (<Redirect onlyState={this.state} to={'/levels'}/>)
+  userValidation(e) {
+    console.log(this.state);
+    console.log("login button is clicked!!");
+    fetch("/auth/users", {
+      method: "POST",
+      body: JSON.stringify({ data: this.state }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(body => {
+        console.log(body[0].userName);
+        console.log(this.state.userName);
+        if (body[0].userName) {
+          this.setState({ redirect: true });
+          console.log(this.state);
+          localStorage.setItem(
+            "userData",
+            JSON.stringify({ name: body[0].userName })
+          );
         }
-        return( 
-            <div>
-                <p>Login</p>
-                <input type="text" placeholder="Email" value={this.state.userName} onChange={this.setName.bind(this)}/>
-                <br/>
-                <input type="password" placeholder="Password" value={this.state.password} onChange={this.setPassword.bind(this)}/>
-                <p>Forgot Password?<span><button onClick={this.userValidation.bind(this)}>Login</button></span></p>
-            </div>
-        )
+      })
+      .then(
+        this.setState({
+          userName: "",
+          password: ""
+        })
+      );
+  }
+
+  render() {
+    if (this.state.redirect) {
+      console.log("inside redirect");
+
+      return <Redirect onlyState={this.state} to={"/levels"} />;
     }
+    return (
+      <div className="user-login-div">
+        <h2>Login</h2>
+        <div className="input-div">
+          <div className="styled-input">
+            <input
+              type="text"
+              value={this.state.userName}
+              size="20"
+              onChange={this.setName.bind(this)}
+              required
+            />
+            <label>Email</label>
+            <span />
+          </div>
+          <div className="styled-input">
+            <input
+              type="password"
+              value={this.state.password}
+              size="20"
+              onChange={this.setPassword.bind(this)}
+              required
+            />
+            <label>Password</label>
+            <span />
+          </div>
+
+          <div>
+            <span className="login-button-span">
+              <button onClick={this.userValidation.bind(this)}>Login</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignUp;
