786161cb2a1faf070d335028b8952747bcbd29f5
Anmol <anmol.sharma@mountblue.io>
2018-08-11 01:11:15 +0530

    Done import page css

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <nav className="nav-header">
-    <Link to="/import">Home</Link>
       <div>
-      <Link to="/userScript">User script</Link>
-      <Link to="/prettify">prettify code</Link>
+        <Link to="/import">Home</Link>
+      </div>
+      <div>
+        <Link to="/userScript">User script</Link>
+      </div>
+      <div>
+        <Link to="/prettify">prettify code</Link>
       </div>
     </nav>
   );
